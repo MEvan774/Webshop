ac083578c06ed794715d9c8f69a5369251adc593
Anne Bakker <anne.bakker3@hva.nl>
2025-04-23 12:19:56 +0200

    Fix: bugs in CurrentGameComponent

--- src/web/src/components/CurrentGameComponent.ts
+++ src/web/src/components/CurrentGameComponent.ts
@@ -2,15 +2,9 @@ import { GameResult } from "@shared/types";
 import { html } from "@web/helpers/webComponents";
 import { getGameByID } from "@web/services/CurrentGameService";
 
-/**
- * This component demonstrates the use of sessions, cookies and Services.
- *
- * @remarks This class should be removed from the final product!
- */
 export class CurrentGameComponent extends HTMLElement {
     public async connectedCallback(): Promise<void> {
         this.attachShadow({ mode: "open" });
-
         await this.render();
     }
 
@@ -19,71 +13,69 @@ export class CurrentGameComponent extends HTMLElement {
     }
 
     private async render(): Promise<void> {
-        // Get current game
-        const currentGame: GameResult | null = await this.getCurrentGame();
-
-        let element: HTMLElement;
-
         if (!this.shadowRoot) {
             return;
         }
 
+        const currentGame: GameResult | null = await this.getCurrentGame();
+        let element: HTMLElement;
+
         if (currentGame) {
-            // Make a string with the authors
-            let authors: string = "";
+            // Create authors string
+            const authors: string = currentGame.authors
+                .map((author: string, index: number): string =>
+                    index === 0 ? author : `, ${author}`)
+                .join("");
 
-            for (let x: number = 0; x < currentGame.authors.length; x++) {
-                if (x !== 0) {
-                    authors += ", ";
-                }
+            // Create images HTML
+            const imagesHTML: string = currentGame.images
+                .map((image: string): string => `<img src='${image}' alt="Game screenshot">`)
+                .join("");
 
-                authors += currentGame.authors[x];
-            }
-
-            // Make HTML for the images string
-            let imagesHTML: string = "";
-
-            for (let x: number = 0; x < currentGame.images.length; x++) {
-                imagesHTML += "<img src='" + currentGame.images[x] + "'>";
-            }
+            // Create reviews section
+            const reviewsHTML: string = currentGame.reviews.length > 0
+                ? currentGame.reviews.join("<br>")
+                : "This game has no reviews yet.";
 
             element = html`
-            <div>
                 <div>
-                    <h1 id="currentGameName">${currentGame.title}</h1><br>
+                    <div>
+                        <h1 id="currentGameName">${currentGame.title}</h1>
+                        <p>SKU: ${currentGame.SKU}</p><br>
 
-                    <div id="currentGameMainFloat">
-                        <img src="${currentGame.thumbnail}">
+                        <div id="currentGameMainFloat">
+                            <img src="${currentGame.thumbnail}" alt="Game thumbnail">
 
-                        <div id="currentGameTextDiv">
-                            <div id="currentGameText">${currentGame.descriptionHtml}</p><br>
-                            <p>Developers: </p><br>
-                            <p id="currentGameDevelopers">${authors}</p><br>
-                            <div id="currentGameTags">${currentGame.tags}</div><br>
+                            <div id="currentGameTextDiv">
+                                <div id="currentGameText">${currentGame.descriptionHtml}</div><br>
+                                <p>Developers: </p><br>
+                                <p id="currentGameDevelopers">${authors}</p><br>
+                                <div id="currentGameTags">${currentGame.tags.join(", ")}</div><br>
+                                <p>Game URL: <a href="${currentGame.url}" target="_blank">${currentGame.url}</a></p>
+                            </div>
                         </div>
+
+                        <div id="currentGameImagesDiv">${imagesHTML}</div><br>
+
+                        <div id="currentGameButtonsDiv">
+                            <button id="currentGameBuyButton" class="currentGameButtons">Koop nu!</button>
+                            <button id="currentGamePriceButton" class="currentGameButtons">Price</button>
+                            <button id="currentGameHeartButton">Favorites</button>
+                        </div>
+
+                        <h2>Reviews:</h2>
+                        <div id="currentGameReviewsDiv">${reviewsHTML}</div>
                     </div>
-
-                    <div id="currentGameImagesDiv">${imagesHTML}</div><br>
-
-                    <div id="currentGameButtonsDiv">
-                        <button id="currentGameBuyButton" class="currentGameButtons">Koop nu!</button>
-                        <button id="currentGamePriceButton" class="currentGameButtons">Price</button>
-                        <button id="currentGameHeartButton">Favorites</button>
-                    </div>
-
-                    <h2>Reviews:</h2>
-                    
-                    <div id="currentGameReviewsDiv">This game has no reviews yet.</div>
                 </div>
-            </div>
-        </div>
-        `;
+            `;
         }
         else {
-            element = html``;
+            element = html`<div>No game found</div>`;
         }
 
-        this.shadowRoot.firstChild?.remove();
+        if (this.shadowRoot.firstChild) {
+            this.shadowRoot.firstChild.remove();
+        }
         this.shadowRoot.append(element);
     }
 }
