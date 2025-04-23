5617e879ad91e9cd1642080ea38fdfe824cebd7d
Anne Bakker <anne.bakker3@hva.nl>
2025-04-23 20:50:46 +0200

    Fix: bugs voor null

--- src/web/src/components/CurrentGameComponent.ts
+++ src/web/src/components/CurrentGameComponent.ts
@@ -31,7 +31,7 @@ export class CurrentGameComponent extends HTMLElement {
         }
 
         // Create string with authors
-        const authors: string = currentGame.authors.length
+        const authors: string = currentGame.authors?.length
             ? currentGame.authors
                 .map((author: string, index: number): string =>
                     index === 0 ? author : `, ${author}`)
@@ -71,7 +71,7 @@ export class CurrentGameComponent extends HTMLElement {
                             <div id="currentGameText">${currentGame.descriptionHtml}</div><br>
                             <p>Developers: </p><br>
                             <p id="currentGameDevelopers">${authors}</p><br>
-                            <div id="currentGameTags">${currentGame.tags.join(", ")}</div><br>
+                            <div id="currentGameTags">${currentGame.tags?.join(", ")}</div><br>
                         </div>
                     </div>
 
