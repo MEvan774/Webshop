ef0fe93b016b98d761ce6daceb6325ede50fefe8
Francesco Pavlovic <francesco.pavlovic@hva.nl>
2025-05-01 21:57:12 +0200

    fix: CSS to own file

--- src/web/src/components/NavComponent.ts
+++ src/web/src/components/NavComponent.ts
@@ -1,4 +1,6 @@
-import { html, css } from "@web/helpers/webComponents";
+/* Deze navigatiebalk component wordt niet gebruikt in het project. Om toekomstige verwarringen met mergen te voorkomen, refereert dit document naar andere documenten waarin "remove_me!" in de naam staat. */
+
+import { html } from "@web/helpers/webComponents";
 
 export class NavComponent extends HTMLElement {
     public connectedCallback(): void {
@@ -11,114 +13,6 @@ export class NavComponent extends HTMLElement {
             if (!this.shadowRoot) {
                 throw new Error("No ShadowRoot");
             }
-
-            const elementStyle: string = css`
-            @import url('https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200');
-
-            :root {
-                --white: #FFFAF0;
-                --blue: #1C2594;
-                --black: #1B1212;
-                --light-blue: #7883CE;
-                --blue-gradient: linear-gradient(90deg, rgba(28, 37, 148, 1) 0%, rgba(91, 202, 243, 1) 100%);
-            }
-
-            .material-symbols-outlined {
-                 font-variation-settings:
-                         'FILL' 0,
-                         'wght' 400,
-                         'GRAD' 0,
-                         'opsz' 24
-            }
-
-            .pacifico-regular {
-                font-family: "Pacifico", cursive;
-                font-weight: 400;
-                font-style: normal;
-            }
-
-            .material-symbols-outlined {
-                font-variation-settings:
-                    'FILL' 0,
-                    'wght' 400,
-                    'GRAD' 0,
-                    'opsz' 24;
-                font-size: 50px;
-            }
-
-            html, body {
-                margin: 0;
-                padding: 0;
-                font-family: Pacifico, cursive;
-                color: var(--white);
-            }
-
-            nav {
-                display: flex;
-                flex-grow: 1;
-                justify-content: space-between;
-                align-items: center;
-                background: var(--blue-gradient);
-            }
-
-            #big-logo, #logo {
-                margin: 0;
-            }
-
-            #big-logo {
-                margin-left: 20px;
-                text-decoration: none;
-                color: var(--white);
-            }
-
-            #logo {
-                display: flex;
-                gap: 5px;
-            }
-
-            #logo h1 {
-                margin: 0;
-                font-size: 45px;
-            }
-
-            #logo img {
-                width: 38px;
-                height: 64px;
-            }
-
-            #search-bar {
-                position: relative;
-            }
-
-            #search-bar input {
-                font-family: "Raleway", sans-serif;
-                width: 525px;
-                height: 50px;
-                border-radius: 10px;
-                background-image: url("/assets/img/search_icon.png");
-                background-repeat: no-repeat;
-                background-position: 6px center;
-                padding-left: 40px;
-                font-size: 28px;
-            }
-
-            #button-container {
-                margin-right: 20px;
-            }
-
-            #button-container a {
-                margin-left: 20px;
-            }
-
-            #button-container > button {
-                width: 167px;
-                height: 50px;
-                font-size: 26px;
-                border-radius: 2px;
-                background-color: var(--white);
-                font-family: "Raleway", sans-serif;
-            }
-            `;
 
             // if () {
             //     element = html`
@@ -145,7 +39,6 @@ export class NavComponent extends HTMLElement {
             // else {
             const element: HTMLElement = html`
                 <nav>
-                    <style>${elementStyle}</style>
                     <a id="big-logo" href="index.html">
                         <div id="logo">
                             <img src="/assets/img/logo_simple.png" alt="LucaStars Logo">
@@ -167,8 +60,13 @@ export class NavComponent extends HTMLElement {
                 `;
             // }
 
+            const styleLink: HTMLLinkElement = document.createElement("link");
+            styleLink.setAttribute("rel", "stylesheet");
+            styleLink.setAttribute("href", "/assets/css/nav(remove_me!).css");
+
             this.shadowRoot.firstChild?.remove();
             this.shadowRoot.append(element);
+            this.shadowRoot.appendChild(styleLink);
         }
         catch (error) {
             console.error(error);
